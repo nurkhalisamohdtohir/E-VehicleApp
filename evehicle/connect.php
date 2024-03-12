8655880bdd1b378238072e8794978eb01188fea2
nurkhalisa <lisatohir@gmail.com>
2024-03-12 12:16:43 +0800

    Deploy to vercel

--- evehicle/connect.php
+++ evehicle/connect.php
@@ -1,8 +1,8 @@
 <?php
-$dbhost = "localhost";
-$dbuser = "root";
-$dbpass = "";
-$dbname = "evehicle";
+$dbhost = "john.db.elephantsql.com";
+$dbuser = "qghjezet";
+$dbpass = "qoCgxPC3oA-jnIzkKZgAdtGDHjbwK72Z";
+$dbname = "qghjezet";
 $conn = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname) or die('cannot connect to the server'); 
 //mysqli_select_db() or die('database selection problem');
 ?>
